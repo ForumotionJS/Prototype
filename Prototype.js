d1b2dc55aa4e884bbb1f960f6c747394fd4605af
Siti Almeida Maryam <horpdorpclop@gmail.com>
2015-06-04 20:39:11 +0800

    Initial release

---
+++ Prototype.js
@@ -0,0 +1,15 @@
+/*
+* - Prototype -
+* Changelog:
+* 04 June 2015: Initial release.
+* =================== Description =====================
+* Prototype would adds a few non-existing prototype in JavaScript to help you spice up things.
+*/
+
+Array.prototype.empty = function(arr) {
+ for(var i=0; i<arr.length; i++) {
+  arr.pop();
+ if(typeof arr !== 'array') {
+  console.log("There's a problem on running the code. Please contact the site administrator.");
+ }
+};
